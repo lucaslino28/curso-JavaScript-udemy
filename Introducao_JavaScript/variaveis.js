2df20ef0290949ef027f68ad2f74e49e13121584
lucaslino28 <lucaslino2804@gmail.com>
2026-04-25 22:49:17 -0300

    Aprendendo sobre variáveis

---
+++ Introducao_JavaScript/variaveis.js
@@ -0,0 +1,12 @@
+let nome = "Lucas";
+
+/* 
+Declarando a variavel utilizando let (mutável), o valor da variável poder ser subscrito, 
+ou você pode alterar o tipo do valor da variável.
+*/
+
+nome = "Maria";
+
+nome = 10;
+
+console.log(nome);
