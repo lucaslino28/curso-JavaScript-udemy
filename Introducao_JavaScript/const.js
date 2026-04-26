0859de474a4119d8ff0c826249142c1a5d9d413c
lucaslino28 <lucaslino2804@gmail.com>
2026-04-25 23:44:23 -0300

    Aprendendo sobre variáveis constantes e boas práticas de declaração

---
+++ Introducao_JavaScript/const.js
@@ -0,0 +1,16 @@
+// utilizando const o valor não pode ser alterado.
+const nome = "João";
+
+console.log(nome);
+
+// camel case
+const nomeCompleto = "João da Silva";
+
+console.log(nomeCompleto);
+
+// DICAS SOBRE DECLARAÇÃO DE VARIÁVEIS:
+// usar nomes descritivos
+// evitar abreviacoes
+// evitar nomes grandes demais
+
+
