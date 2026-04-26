e0387f0654a2e045c33f9f98ce4f6704d8749f3b
lucaslino28 <lucaslino2804@gmail.com>
2026-04-25 23:12:30 -0300

    Aprendendo sobre os tipos de dados primitivos

---
+++ Introducao_JavaScript/tipos-de-dados.js
@@ -0,0 +1,40 @@
+// tipos primitivos
+
+// numero - number
+let numero = 10;
+let numero2 = 9.5;
+
+console.log(typeof numero);
+console.log(typeof numero2);
+
+// string
+let nome = "Lucas";
+
+console.log(typeof nome);
+
+// boolean
+let ligado = true;
+
+console.log(typeof ligado);
+
+// undefined - não tem valor definido.
+let teste = undefined;
+let teste2;
+
+console.log(typeof teste);
+console.log(typeof teste2);
+
+// null - guarda a váriavel, mas deixa o espaço reservado. Para futuramente colocar valor.
+let time = null;
+
+console.log(typeof time);
+
+// object
+let objeto = {}
+
+console.log(typeof objeto);
+
+// function
+let funcao = () => {};
+
+console.log(typeof funcao);
