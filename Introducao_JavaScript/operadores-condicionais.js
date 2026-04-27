1777454a15be9d1c759dda225510a5b2ee68ef30
lucaslino28 <lucaslino2804@gmail.com>
2026-04-27 18:39:39 -0300

    Aprendendo quais são os operadores condicionais

---
+++ Introducao_JavaScript/operadores-condicionais.js
@@ -0,0 +1,29 @@
+/*
+== igual a (valor)
+=== Estritamente igual a (valor e tipo)
+!= Diferente de
+> Maior que
+>= Maior ou igual que
+< Menor que
+<= Menor ou igual que
+*/ 
+
+let num1 = 18;
+let num2 = 99;
+
+let resultado = (num1 == num2);
+let resultado1 = num1 != num2;
+let resultado2 = num1 > num2;
+let resultado3 = num1 >= num2;
+let resultado4 = num1 < num2;
+let resultado5 = num1 <= num2;
+let resultado6 = (num1 === num2); // Usado para verificar a igualde de tipo e valor.      
+
+console.log(resultado);
+console.log(resultado1);
+console.log(resultado2);
+console.log(resultado3);
+console.log(resultado4);
+console.log(resultado5);
+console.log(resultado6);
+
