f7959563fa489e69fd443dcaab3c08db201fe4e9
lucaslino28 <lucaslino2804@gmail.com>
2026-05-08 18:07:59 -0300

    Nessa aula aprendemos sobre a estrutura de Switch case.

---
+++ Introducao_JavaScript/switch-case.js
@@ -0,0 +1,29 @@
+// Switch case
+
+const diaDaSemana = "Sexta";
+
+switch (diaDaSemana) {
+    case "Domingo":
+        console.log("Dia 1 - Domingo");
+        break;
+    case "Segunda":
+        console.log("Dia 2 - Segunda");
+        break;
+    case "Terça":
+        console.log("Dia 3 - Terça");
+        break;
+    case "Quarta":
+        console.log("Dia 4 - Quarta");
+        break;
+    case "Quinta":
+        console.log("Dia 5 - Quinta");
+        break;
+    case "Sexta":
+        console.log("Dia 6 - Sexta");
+        break;
+    case "Sábado":
+        console.log("Dia 7 - Sábado");
+        break;
+    default:
+        console.log("Dia ainda não existe.");
+}
